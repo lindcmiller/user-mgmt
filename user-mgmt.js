f80de6be2e2b17d719addd13b27d8c28ed559cf6
Lindsey Miller <lindc.miller@gmail.com>
2015-05-21 15:57:45 -0400

    initial commit

---
+++ user-mgmt.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+    $('.addUserButton').click(function () {
+      this.preventDefault();
+
+	var userArray = [];
+}
+
+$ ('userArray[]').create('.userList') {
+	.attr('<li>' + '' + '</li>');
+	userArray.append(['ul']);
+ };
+
+$('addUser[]').on('input', function () {
+    var input = $(this);
+    var len = input.val();
+
+    var container = input.parent();
+      //span.appendTo(container);
+      container.append(span);
+    });
+});
